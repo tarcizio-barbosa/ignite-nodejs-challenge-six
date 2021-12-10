e03e9da95b7feb72f8eb44c8b36b8597ac83f0c9
Tarcizio Barbosa - Dislog <tarcizio.barbosa@outlook.com>
2021-12-10 11:24:34 -0300

    finish authenticate user tests

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUs
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError";
 
 let inMemoryUsersRepository: InMemoryUsersRepository;
 let createUserUseCase: CreateUserUseCase;
@@ -31,5 +32,39 @@ describe("Authenticate a User", () => {
     });
 
     expect(response).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate a User with the incorrect E-mail", () => {
+    const newUser: ICreateUserDTO = {
+      name: "Tarcizio",
+      email: "tarcizio@io.com.br",
+      password: "k9sonwow1%",
+    };
+
+    expect(async () => {
+      await createUserUseCase.execute(newUser);
+
+      await authenticateUserUseCase.execute({
+        email: "incorrect@.io.com",
+        password: newUser.password,
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  });
+
+  it("Should not be able to authenticate a User with the incorrect Password", () => {
+    const newUser: ICreateUserDTO = {
+      name: "Tarcizio",
+      email: "tarcizio@io.com.br",
+      password: "k9sonwow1%",
+    };
+
+    expect(async () => {
+      await createUserUseCase.execute(newUser);
+
+      await authenticateUserUseCase.execute({
+        email: newUser.email,
+        password: "k9sonwow2%",
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
   });
 });
