698c3a04d09a4d554bfefd5ab22fd7491cffa2a7
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-12-13 20:34:23 -0300

    finish get balance tests

--- src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -1,7 +1,51 @@
-describe("Get Balance", () => {
-  it("Should be able to get a User balance", () => {
-    const balance = {};
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+import { v4 as uuid } from "uuid";
+import { GetBalanceError } from "./GetBalanceError";
 
-    expect(balance).toHaveProperty("balance");
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+
+let getBalanceUseCase: GetBalanceUseCase;
+
+describe("Get Balance", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+
+    inMemoryStatementsRepository = new InMemoryStatementsRepository();
+
+    getBalanceUseCase = new GetBalanceUseCase(
+      inMemoryStatementsRepository,
+      inMemoryUsersRepository
+    );
+  });
+
+  it("Should be able to get a User balance", async () => {
+    const newUser = await createUserUseCase.execute({
+      name: "Tarcizio",
+      email: "tarcizio@io.com.br",
+      password: "k9sonwow1%",
+    });
+
+    const getBalance = await getBalanceUseCase.execute({
+      user_id: newUser.id as string,
+    });
+
+    expect(getBalance).toHaveProperty("balance");
+  });
+
+  it("Should not ble able to get a balance if user does not exists", () => {
+    const id = uuid();
+
+    expect(async () => {
+      await getBalanceUseCase.execute({
+        user_id: id,
+      });
+    }).rejects.toBeInstanceOf(GetBalanceError);
   });
 });
